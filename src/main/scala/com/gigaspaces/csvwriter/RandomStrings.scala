6229801ddf84f5607eb3e08505c538bc2ba4cae6
Jason Nerothin <jason.nerothin@kohls.com>
2014-04-01 16:40:16 -0500

    Adding scaladoc

--- src/main/scala/com/gigaspaces/csvwriter/RandomStrings.scala
+++ src/main/scala/com/gigaspaces/csvwriter/RandomStrings.scala
@@ -2,14 +2,24 @@ package com.gigaspaces.csvwriter
 
 import scala.util.Random
 
+/**
+  * A mixin for generating random strings.
+  */
 trait RandomStrings {
 
   private val rand = new Random(System.currentTimeMillis())
 
+  /**
+    * @return an alphabetic character
+    */
   def randChar(): Character = {
     (rand.nextInt(26) + 65).toChar
   }
 
+  /**
+    * @param len length of returned string
+    * @return a random string, length len, comprised of alphabetic characters
+    */
   def randString(len: Int): String = {
     val builder = new StringBuilder
     for (i <- 1 to len ) builder.append(randChar())
