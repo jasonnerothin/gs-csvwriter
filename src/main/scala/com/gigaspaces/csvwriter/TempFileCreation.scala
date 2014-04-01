9ebd9762e5e71a181ee919e6dba4cd89a043a475
Jason Nerothin <jason.nerothin@kohls.com>
2014-04-01 16:44:01 -0500

    Adding scaladoc

--- src/main/scala/com/gigaspaces/csvwriter/TempFileCreation.scala
+++ src/main/scala/com/gigaspaces/csvwriter/TempFileCreation.scala
@@ -2,15 +2,29 @@ package com.gigaspaces.csvwriter
 
 import java.io.File
 
+/**
+  * A trait for generating files in the temp directory, sometimes temporary in nature.
+  */
 trait TempFileCreation extends RandomStrings {
 
-  protected def useSlashTmpInstead = true
+  /** @return a convenience feature, which makes it a lot easier to looking at temp files during unit testing
+    */
+  protected def useSlashTmpInstead = false
 
+  /**
+    * @return location of the temp directory
+    */
   def tempDir(): File = {
     if (!useSlashTmpInstead) return new File(System.getProperty("java.io.tmpdir"))
     new File("/tmp")
   }
 
+  /**
+    * Creates a new (empty) file in the temp directory. Optionally, it's temporary.
+    * @param fileNameLen length of the (random) file name to use
+    * @param deleteLater by default, random files in the temp directory are ''not'' deleted
+    * @return a reference to the file.
+    */
   def newRandFile(fileNameLen: Int, deleteLater: Boolean = false): File = {
     var aFile = randFile(fileNameLen)
     while (!aFile.createNewFile) aFile = randFile(fileNameLen)
