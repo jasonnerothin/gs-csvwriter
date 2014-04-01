966ebcf61d4bfb971519fbaf3f5678932bcd1770
Jason Nerothin <jason.nerothin@kohls.com>
2014-04-01 01:14:09 -0500

    working CL parser

---
+++ src/main/scala/com/gigaspaces/csvwriter/CommandLineProcessing.scala
@@ -0,0 +1,56 @@
+package com.gigaspaces.csvwriter
+
+import java.io.File
+import org.apache.commons.cli.{CommandLine, GnuParser, Options}
+import scala.Predef.String
+import scala.Predef.require
+import org.openspaces.core.space.UrlSpaceConfigurer
+import org.openspaces.core.{GigaSpace, GigaSpaceConfigurer}
+
+class CommandLineProcessing(args: Array[String]) {
+
+  private val inputFileOption = "in"
+  private val inputFileDescription = "csv file"
+  private val spaceUrlOption = "url"
+  private val spaceUrlDescription = "Space URL (see http://wiki.gigaspaces.com/wiki/display/XAP9/Space+URL for details)"
+
+  private val options: Options = new Options()
+    .addOption(inputFileOption, true, inputFileDescription)
+    .addOption(spaceUrlOption, true, spaceUrlDescription)
+
+  /** @return parse the command line, and return a java.io.File according to the args
+    */
+  def inputFile(): File = {
+    val p = inputFilePath
+    val f = new File(p)
+    require(!f.isDirectory, String.format("Input %s must not be a directory, but [%s] is one.", inputFileDescription, p))
+    require(f.isFile, String.format("Input %s must exist, but [%s] does not.", inputFileDescription, p))
+    f
+  }
+
+  private def inputFilePath: String = stringValFromOpt(inputFileOption)
+
+  private def commandLine(): CommandLine = {
+    require(args != null, options.toString)
+    require(args.length > 1, options.toString)
+    val parser = new GnuParser
+    parser.parse(options, args)
+  }
+
+  private def spaceUrlString: String = stringValFromOpt(spaceUrlOption)
+
+  private def stringValFromOpt(opt: String) = commandLine().getOptionValue(opt)
+
+  /** @return a GigaSpace, properly construed
+    */
+  def gigaSpace(): GigaSpace = {
+
+    val url = spaceUrlString
+    require(url != null, options.toString)
+    require(url.length > 0, options.toString)
+
+    new GigaSpaceConfigurer(new UrlSpaceConfigurer(url)).gigaSpace()
+
+  }
+
+}
---
+++ src/test/scala/com/gigaspaces/csvwriter/CommandLineProcessingSuite.scala
@@ -0,0 +1,105 @@
+import com.gigaspaces.csvwriter.CommandLineProcessing
+import java.io.{FileWriter, File}
+import org.openspaces.core.space.CannotCreateSpaceException
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import scala.util.Random
+
+class CommandLineProcessingSuite extends FunSuite with BeforeAndAfterEach {
+
+  val tempDir: File = new File("/tmp")
+  val aDirectoryPath: File = tempDir
+
+  var notAFile: File = null
+  var aFile: File = null
+
+  val rand = new Random(System.currentTimeMillis())
+
+  val mtUrl = ""
+  var garbageUrl: String = ""
+
+  def testInstance(in: String, url: String) = new CommandLineProcessing(Array("-in", in, "-url", url))
+
+  override def beforeEach(): Unit = {
+    aFile = newRandFile(8)
+    notAFile = newRandFile(12)
+    notAFile.delete()
+    garbageUrl = String.format("%s", randString(6))
+  }
+
+  override def afterEach(): Unit = {
+    aFile.deleteOnExit()
+    notAFile.deleteOnExit()
+    garbageUrl = ""
+  }
+
+  private def newRandFile(fileNameLen: Int): File = {
+    def randFile = new File(tempDir, randString(fileNameLen))
+    val aFile = randFile
+    //    println(String.format("Creating file: %s", aFile.getAbsolutePath))
+    aFile.createNewFile
+    writeTo(aFile)
+    aFile
+  }
+
+  private def writeTo(someFile: File) = {
+    val writer = new FileWriter(someFile)
+    writer.write("foo")
+    writer.write("\n")
+    writer.close()
+  }
+
+  private def randChar(): Character = {
+    (rand.nextInt(26) + 65).toChar
+  }
+
+  private def randString(len: Int): String = {
+    val builder = new StringBuilder
+    for (i <- 0 to len) builder.append(randChar())
+    builder.toString()
+  }
+
+  test("inputFile -in aFilePath returns a file reference") {
+    val actual = testInstance(in = aFile.getAbsolutePath, url = randString(4)).inputFile()
+    assert(actual.isFile)
+  }
+
+  test("inputFile -in notAFilePath throws") {
+    try {
+      testInstance(in = notAFile.getAbsolutePath, url = randString(4)).inputFile()
+    } catch {
+      case e: IllegalArgumentException =>
+      case e: Throwable => fail("Expected IllegalArgumentException instead.", e)
+    }
+
+  }
+
+  test("inputFile -in aDirectoryPath throws") {
+    try {
+      testInstance(in = aDirectoryPath.getAbsolutePath, url = randString(4)).inputFile()
+    } catch {
+      case e: IllegalArgumentException =>
+      case e: Throwable => fail("Expected IllegalArgumentException instead.", e)
+    }
+  }
+
+  test("gigaSpace -url mtUrl throws") {
+    try {
+      val actual = testInstance(url = mtUrl, in = randString(4))
+      actual.gigaSpace()
+    } catch {
+      case e: IllegalArgumentException =>
+      case e: Throwable => fail("Expected IllegalArgumentException instead.", e)
+    }
+  }
+
+  test("gigaSpace -url garbageUrl throws") {
+    try {
+      val actual = testInstance(url = randString(4), in = aFile.getAbsolutePath)
+      actual.gigaSpace()
+    } catch {
+      case e: CannotCreateSpaceException =>
+      case e: Throwable => fail("Expected CannotCreateSpaceException instead.", e)
+    }
+  }
+
+}
