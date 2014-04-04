5aebc4fbc00ca19005df20727ce82b298dba7337
Jason Nerothin <jason.nerothin@kohls.com>
2014-04-04 09:46:17 -0500

    Finally got a reasonbly written (failing) test

--- src/main/scala/com/gigaspaces/csvwriter/TempFileUtf8Encoder.scala
+++ src/main/scala/com/gigaspaces/csvwriter/TempFileUtf8Encoder.scala
@@ -3,28 +3,28 @@ package com.gigaspaces.csvwriter
 import java.io._
 import java.nio.charset.Charset
 
-class TempFileUtf8Encoder(clp: CommandLineProcessing) extends EncodingDetection with TempFileCreation {
+class TempFileUtf8Encoder(clp: CommandLineProcessing, encodingDetection: EncodingDetection, tempFileCreation: TempFileCreation) {
 
-  private val filenameLen = 12
   private val utf8Charset = Charset.forName("UTF-8")
   private val bufferSize = 4096
+  private val filenameLen = 12
 
   /** Generates a temp file containing the contents of the [[com.gigaspaces.csvwriter.CommandLineProcessing]]
     * input file, encoded in UTF-8
     * @return a reference to the file in temp
     */
-  def encodeToTemp(): File = {
+  def encodeToTemp(deleteTempFile: Boolean = true): File = {
 
     val src = clp.inputFile()
-    val inputCharset = detectEncoding(src)
+    val inputCharset = encodingDetection.detectEncoding(src)
     val reader = new InputStreamReader(new FileInputStream(src), inputCharset)
 
     val inputBuffer = new Array[Char](bufferSize)
 
-    val dest = newRandFile(filenameLen, deleteLater = true)
+    val dest = tempFileCreation.newRandFile(filenameLen, deleteLater = deleteTempFile)
     val writer = new OutputStreamWriter(new FileOutputStream(dest), utf8Charset)
 
-    while(reader.ready()) {
+    while (reader.ready()) {
       val numBytesRead = reader.read(inputBuffer)
       writer.write(inputBuffer, 0, numBytesRead)
     }
--- src/test/scala/com/gigaspaces/csvwriter/EncodingDetectionSuite.scala
+++ src/test/scala/com/gigaspaces/csvwriter/EncodingDetectionSuite.scala
@@ -1,32 +1,90 @@
 package com.gigaspaces.csvwriter
 
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
-import java.io.File
-import java.nio.charset.Charset
+import java.io._
 import org.slf4j.{Logger, LoggerFactory}
+import java.nio.charset.Charset
 
 class EncodingDetectionSuite extends FunSuite with EncodingDetection with TempFileCreation with BeforeAndAfterAll {
 
-  private val logger: Logger = LoggerFactory.getLogger(getClass)
+  val logger: Logger = LoggerFactory.getLogger(getClass)
 
-  ignore("detect windows 1252") {
+  val cp1252ToUnicodeByteMap = Map[Char, Char](0x0080.toChar -> 0x20ac.toChar, 0x0082.toChar -> 0x201a.toChar)
+  val cp1252BytesWithoutUnicodeAnalogues = List[Char](0x0081.toChar, 0x008d.toChar)
 
-    val win1252bytes = 0x80
-    val utf8bytes = 0x80
+  test("detect windows 1252") {
 
-    val testFile = newRandFile(10, deleteLater = true)
+    val winFile = newRandFile(6, deleteLater = true)
+
+    val writer = new BufferedWriter(new FileWriter(winFile))
+    writer.write(cp1252String)
+    writer.write("\n")
+    writer.close()
+
+    val str = cp1252String
+
+    logger.trace("Special string is [{}].", str)
+    val encoding = detectEncoding(winFile)
+    logger.debug("Encoding is {}.", encoding.displayName)
+
+    assert("windows-1252" === encoding.displayName)
 
   }
 
-  test("a real file comes back with default encoding") {
+  /** The special bytes in this string have a code-point in cp 1252 but
+    * have no code-point in UTF-8 or UTF-16
+    * @return the String containing bytes: ["hello",0x0081,0x008d,"earth"]
+    */
+  def cp1252String = {
 
-    val testFile = new File(new File("."), "/src/test/resources/100Rows.txt")
-    logger.debug("Test file = {}", testFile.getAbsolutePath)
+    val hi = "hello"
+    val planet = "earth"
 
-    val encoding = detectEncoding(testFile)
-    logger.debug("Encoding is {}", encoding.displayName())
+    var chars = Array[Char]()
+    for (b <- hi) chars = chars :+ b
+    chars = chars :+ cp1252BytesWithoutUnicodeAnalogues(0)
+    chars = chars :+ cp1252BytesWithoutUnicodeAnalogues(1)
+    for (b <- planet) chars = chars :+ b
 
-    assert(Charset.defaultCharset() == encoding)
+    // even though we're encoding it in "Unicode", it contains two
+    // bytes that are present in cp1252 (Windows-1252) that do
+    // *not* have a corresponding code point in UTF-16BE - i.e.
+    // this is a "Windows-1252" string
+
+    var bytes = Array[Byte]()
+    for (ch <- chars) bytes = bytes ++ asBytes(ch)
+
+    new String(bytes, "UTF-16BE")
+
   }
+
+  def asBytes(ch: Char): Array[Byte] = {
+    Array[Byte]((ch / 256).toByte, ch.toByte)
+  }
+
+  test("asBytes works") {
+    val ch = 'a'
+    val arr = asBytes(ch)
+
+    val b0 = arr(0)
+    val b1 = arr(1)
+
+    val ch0 = b0.toChar
+    val ch1 = b1.toChar
+
+    assert(ch0 === 0x00)
+    assert(ch1 === 'a')
+  }
+
+    test("a real file comes back with default encoding") {
+
+      val testFile = new File(new File("."), "/src/test/resources/100Rows.txt")
+      logger.debug("Test file = {}", testFile.getAbsolutePath)
+
+      val encoding = detectEncoding(testFile)
+      logger.debug("Encoding is {}", encoding.displayName())
+
+      assert(Charset.defaultCharset() == encoding)
+    }
 
 }
